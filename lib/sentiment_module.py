ca4338450ab665229bcc557106deb7ca6bc3f963
LucasPham <ldp43@case.edu>
2020-07-26 03:07:05 -0400

    jul26

--- lib/sentiment_module.py
+++ lib/sentiment_module.py
@@ -4,10 +4,14 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-def tokenize_stem(string):
+def cleaning_data(string):
     review = re.sub('[^a-zA-Z]', ' ', string)
-    review = review.lower()
-    review = review.split()
+    review = review.lower().split()
+    return review
+
+
+def tokenize_stem(string):
+    review = cleaning_data(string)
     ps = PorterStemmer()
     all_stopwords = stopwords.words('english')
     all_stopwords.remove('not')
@@ -16,10 +20,21 @@ def tokenize_stem(string):
 
     return review
 
-# corpus = crop_text("Business live  Markets hope for progress over EU recovery fund; US consumer confidence drops â€“ as it happened")
-# print(corpus)
+def cluster_extraction(nn_output):
+    sentiments = []
+    for item in nn_output:
+        for i in range(0,3):
+            if item[i] == max(item):
+                sentiments.append(i)
+    return sentiments
 
-# from sklearn.feature_extraction.text import CountVectorizer
-# cv = CountVectorizer(max_features = 1500)
-# X = cv.fit_transform([corpus]).toarray()
-# print(X)
+def combine_sentiments(sentiments1, sentiments2):
+    mutual_sentiments = []
+    for s1, s2 in zip(sentiments1, sentiments2):
+        if s1 +s2 > 2:
+            mutual_sentiments.append(2)
+        elif s1 + s2 == 2:
+            mutual_sentiments.append(1)
+        elif s1 + s2 < 2:
+            mutual_sentiments.append(0)
+    return mutual_sentiments
